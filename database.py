59b95fbe676ce07d7c534627591d102f01e7c558
tech_aware <techaware.ai@gmail.com>
2024-06-15 13:51:15 +0200

    updating database to accept event_name and event_date in an event table

--- database.py
+++ database.py
@@ -1,21 +1,28 @@
 import sqlite3
 import logging
 
-
-# Function to initialize the database
 def initialize_database(db_path):
     try:
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
 
-        # Create tables if they don't exist
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS event (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                event_name TEXT,
+                event_date TEXT
+            )
+        ''')
+        logging.info("Table 'event' initialized or already exists.")
+
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS receipts (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                event_id INTEGER,
                 date TEXT,
                 fournisseur TEXT,
                 localisation TEXT,
-                categorie TEXT
+                FOREIGN KEY (event_id) REFERENCES event(id)
             )
         ''')
         logging.info("Table 'receipts' initialized or already exists.")
@@ -24,6 +31,8 @@ def initialize_database(db_path):
             CREATE TABLE IF NOT EXISTS articles (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 receipt_id INTEGER,
+                famille TEXT,
+                sous_famille TEXT,
                 nom TEXT,
                 prix_unitaire REAL,
                 quantite REAL,
@@ -39,39 +48,52 @@ def initialize_database(db_path):
     except Exception as e:
         logging.error(f"Error initializing database: {e}")
 
-
-# Function to insert receipt data
-def insert_receipt_data(db_path, receipt_data):
+def insert_receipt_data(db_path, receipt_data, event_id):
     try:
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
 
         cursor.execute('''
-            INSERT INTO receipts (date, fournisseur, localisation, categorie)
+            INSERT INTO receipts (event_id, date, fournisseur, localisation)
             VALUES (?, ?, ?, ?)
         ''', (
-        receipt_data['date'], receipt_data['fournisseur'], receipt_data['localisation'], receipt_data['categorie']))
+        event_id, receipt_data['date'], receipt_data['fournisseur'], receipt_data['localisation']))
 
         receipt_id = cursor.lastrowid
         logging.info(
-            f"Inserted receipt with ID {receipt_id}: Date: {receipt_data['date']}, Fournisseur: {receipt_data['fournisseur']}, Localisation: {receipt_data['localisation']}, Catégorie: {receipt_data['categorie']}")
+            f"Inserted receipt with ID {receipt_id}: Date: {receipt_data['date']}, Fournisseur: {receipt_data['fournisseur']}, Localisation: {receipt_data['localisation']}")
 
         for article in receipt_data['articles']:
             cursor.execute('''
-                INSERT INTO articles (receipt_id, nom, prix_unitaire, quantite, prix_total)
-                VALUES (?, ?, ?, ?, ?)
+                INSERT INTO articles (receipt_id, famille, sous_famille, nom, prix_unitaire, quantite, prix_total)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
             ''', (
                 receipt_id,
+                article['famille'],
+                article['sous_famille'],
                 article['nom'],
                 float(article['prix_unitaire']),
                 float(article['quantite']),
                 float(article['prix_total'])
             ))
             logging.info(
-                f"Inserted article for receipt ID {receipt_id}: Nom: {article['nom']}, Prix unitaire: {article['prix_unitaire']}, Quantité: {article['quantite']}, Prix total: {article['prix_total']}")
-
+                f"Inserted article for receipt ID {receipt_id}: Famille: {article['famille']}, Sous famille: {article['sous_famille']}, Nom: {article['nom']}, Prix unitaire: {article['prix_unitaire']}, Quantité: {article['quantite']}, Prix total: {article['prix_total']}")
         conn.commit()
         conn.close()
         logging.info("Data inserted successfully.")
     except Exception as e:
         logging.error(f"Error inserting data into database: {e}")
+
+def insert_event(db_path, event_name, event_date):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO event (event_name, event_date)
+            VALUES (?, ?)
+        ''', (event_name, event_date))
+        conn.commit()
+        conn.close()
+        logging.info(f"Event '{event_name}' added successfully.")
+    except Exception as e:
+        logging.error(f"Error inserting event into database: {e}")
